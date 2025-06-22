f0d04b2e28767158a6fd61f52278772d4f992f53
Daniel Hernández <dannhc1234@gmail.com>
2025-06-22 17:38:08 -0600

    Cambios form

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro-Shakti</title>
+    <title>Registro - Shakti</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
@@ -26,20 +26,18 @@
         </div>
         <form class="auth-form" enctype="multipart/form-data">
             <label>Nombre</label>
-            <input type="text" placeholder="Roxana" required />
-            <label>Apellido Paterno</label>
-            <input type="text" placeholder="Pérez" required />
-            <label>Apellido Materno</label>
-            <input type="text" placeholder="López" required />
+            <input type="text" placeholder="Ingrese su(s) nombre(s)" required />
+            <label>Apellidos</label>
+            <input type="text" placeholder="Ingrese sus apellidos" required />
             <label>Nombre de usuario</label>
-            <input type="text" placeholder="Roxrz10" required />
+            <input type="text" placeholder="Ingrese su nombre de usuario" required />
             <label>Correo electrónico</label>
-            <input type="email" placeholder="correo@dominio.com" required />
+            <input type="email" placeholder="Ingrese su correo electrónico" required />
             <label>Contraseña</label>
-            <input type="password" placeholder="••••••••" required />
+            <input type="password" placeholder="Ingrese su contraseña" required />
             <label>Confirmar contraseña</label>
-            <input type="password" placeholder="••••••••" required />
-            <label>Subir documento PDF (Para confirmar identidad)</label>
+            <input type="password" placeholder="Ingrese nuevamente su contraseña" required />
+            <label>Subir documento PDF (para confirmar identidad)</label>
             <input type="file" accept="application/pdf" required />
             <label>Fecha de nacimiento</label>
             <input type="date" required />
